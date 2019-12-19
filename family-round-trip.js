fe52865d885312a5f0d3f4eebc77f46df0445733
Toby Ho <airportyh@gmail.com>
2019-12-19 13:10:05 -0500

    unified family test

--- family-round-trip.js
+++ family-round-trip.js
@@ -2,12 +2,53 @@ const { stringify, parse } = require("./jsonr");
 const fs = require("mz/fs");
 
 async function main() {
-    const content = (await fs.readFile("family.json")).toString();
-    const result = parse(content);
-    // console.log(result.emma.name);
-    const output = stringify(result, "  ");
+    const toby = {
+        name: "Toby"
+    };
 
-    console.log(output);
+    const marty = {
+        name: "Marty"
+    };
+
+    const linus = {
+        name: "Linus"
+    };
+
+    const weilai = {
+        name: "Weilai"
+    };
+
+    const emma = {
+        name: "Emma"
+    };
+
+    const children = [
+        marty, linus, emma
+    ];
+    toby.children = children;
+
+    weilai.children = children;
+
+    toby.spouse = weilai;
+    weilai.spouse = toby;
+    for (let child of toby.children) {
+        child.father = toby;
+        child.mother = weilai;
+    }
+
+    marty.siblings = [linus, emma];
+    linus.siblings = [marty, emma];
+    emma.siblings = [linus, marty];
+
+    const family = {
+        toby, marty, linus, weilai, emma
+    };
+    const version1 = stringify(family, "  ");
+    await fs.writeFile("family.json", version1);
+    console.log("Wrote family.json");
+    const version2 = parse(version1);
+    await fs.writeFile("family2.json", stringify(version2, "  "));
+    console.log("Wrote family2.json");
 }
 
 main().catch((e) => console.log(e.message));
