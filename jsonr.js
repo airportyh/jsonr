9f10b68b0547c8718b00e2b76c5cfe1d5aa05981
Toby Ho <airportyh@gmail.com>
2020-08-14 17:00:21 -0400

    export Def and Ref classes

--- jsonr.js
+++ jsonr.js
@@ -1,5 +1,8 @@
 const { stringify } = require("./lib/stringify");
 const { parse } = require("./lib/fp-parser");
+const { Def, Ref } = require("./lib/refs");
 
 exports.stringify = stringify;
 exports.parse = parse;
+exports.Def = Def;
+exports.Ref = Ref;
